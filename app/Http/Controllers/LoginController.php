c2eb07c888f012540c4ff0b47d9eb13becd2e7d5
StrangerPlay17 <miguel.magana0967@alumnos.udg.mx>
2023-11-17 14:19:28 -0600

    Reset password by Email (State,Login,Register->complete)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Http\Requests\LoginRequest;
 use Dotenv\Exception\ValidationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -14,10 +14,23 @@ class LoginController extends Controller
       return view('Login.login',compact('band')); //Folder and file (Route)
     }
 
-    public function recuperar(){
+    public function recover_show(){ #Show the recover accont 
       return view('Login.forgot');
     }
     
+    public function recover_accont(Request $request){ #Send the request in "recover accont"
+      $request->validate([ //Validate the field
+          'email'=>'required|email',
+      ]);
+
+      $status = Password::sendResetLink($request->only('email')); //Status Email
+       
+      return $status == Password::RESET_LINK_SENT       //Returns if the email could be sent or not
+                 ? back()->with('status',__($status))
+                 : back()->withInput($request->only('email'))
+                         ->withErrors(['email'=>__($status)]);
+
+    }
     public function user_login(LoginRequest $request){ //View Login 
       //$credentials = request()->only('email','password'); //Returns the inputs of the view login in "credentials"
       $remember = request()->filled('remember');    #Transform the checkbox into boolean = checkbox = "on" Then checkbox = "true" 
--- resources/views/Login/reset-password.blade.php
+++ resources/views/Login/reset-password.blade.php
@@ -19,9 +19,10 @@
     <section class="login-section">
         <div class="form-box"></div>
             <div class="form-value">
-                <form method="POST" action="#">
-                    @csrf
-                    <h1 class="login-h1"> Inicio </h1>
+                <form method="POST" action="{{route('password.update')}}">
+                    @csrf 
+                    <input type="hidden" name ="token" value="{{ $request->route('token') }}"> {{-- Token --}}
+                    <h1 class="login-h1"> Restablecer contraseña </h1>
                     <div class="inputbox">
                         <ion-icon name="mail-outline">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="44" height="44" viewBox="0 0 24" stroke-width="1.5" stroke="#ffae00" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -30,8 +31,8 @@
                                 <path d="M3 7l9 6l9 -6" />
                             </svg>
                         </ion-icon>
-                        <input name="email" type="email" required autofocus value="{{old('email')}}">
-                        <label for=""> Usuario </label>
+                        <input name="email" type="email" required value="{{old('email',$request->email)}}">
+                        <label for=""> Correo electrónico </label>
                         @error('email')  {{-- Checks if there has been an error in the "name" field --}}
                         <span>*{{$message}}</span> {{--print a message if there is an error--}}
                         <br>
@@ -47,37 +48,30 @@
                                 <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
                             </svg>
                         </ion-icon>
-                        <input name="password" type="password" required>
+                        <input name="password" type="password" required autofocus>
                         <label for=""> Contraseña </label>
                         @error('password')  {{-- Checks if there has been an error in the "name" field --}}
                         <span>*{{$message}}</span> {{--print a message if there is an error--}}
                         <br>
                         @enderror
                     </div>
-                    <div class="forget">
-                        <label for=""><input type="checkbox" name ="remember">Recordar</label>
-                        <a href="{{route('login.recover')}}">Olvidé la Contraseña</a>
+                    <div class="inputbox">
+                        <ion-icon name="lock-closed-outline">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-lock" width="44" height="44" viewBox="0 0 24" stroke-width="1.5" stroke="#ffae00" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6z" />
+                                <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
+                                <path d="M8 11v-4a4 4 0 1 1 8 0v4" />
+                            </svg>
+                        </ion-icon>
+                        <input name="confirmpassword" type="password" required>
+                        <label for=""> Confirmar contraseña </label>
+                        @error('confirmpassword')  {{-- Checks if there has been an error in the "name" field --}}
+                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
+                        <br>
+                        @enderror
                     </div>
-                    <button class="login-button" type="submit"> Iniciar Sesión </button>
-                    <div class="register">
-                        <p>No tengo una cuenta <a href="/register">Registrarse<br><br></a>
-                        </p>
-                    </div> 
-                    <h4 class="credential-h4">
-                        @if ($band)
-                        >Sistema: Credenciales Incorrectas  
-                        @endif
-                        @if(session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                        @endif
-                    <h4> 
-                    @if (Session::has('registro_exitoso'))
-                           <div class="alert alert-success">
-                                {{ Session::get('registro_exitoso') }}
-                           </div>
-                    @endif
+                    <button class="login-button" type="submit"> Restablecer </button>
                 </form>
             </div>
     </section>
