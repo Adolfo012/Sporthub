f82576e2a660e090fec71497f9595b43165c2ce0
Christian Muñoz <chgeovany.15@gmail.com>
2023-11-22 21:53:56 -0600

    Elimine los condicionales de errores en el HTML

--- resources/views/Login/form.blade.php
+++ resources/views/Login/form.blade.php
@@ -28,34 +28,18 @@
                     <div class="inputbox">
                         <input name="name" type="text" required value="{{old('name')}}" autofocus> <br>
                         <label for=""> Nombre </label>
-                        @error('name')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span class="error-message">*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <div class="inputbox">
                         <input name="fsurname" type="text" required value="{{old('fsurname')}}"> <br>
                         <label for=""> Apellido Paterno </label>
-                        @error('fsurname')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <div class="inputbox">
                         <input name="msurname" type="text" required value="{{old('msurname')}}"> <br>
                         <label for=""> Apellido Materno </label>
-                        @error('msurname')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <div class="inputbox-birthday">
                         <label class="birthday">Fecha de Nacimiento</label>
                         <input name="birthdate" type="date" required value="{{old('birthdate')}}"> <br>
-                        @error('birthdate')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <div class="inputbox-gender">
                         <label class="genero" >Género</label>
@@ -67,42 +51,22 @@
                             <input type="radio" id="non-binary" name="gender" value="non-binary" required value="{{old('gender')}}"> <br>
                             <label for="non-binary"><br><br>Sin mencionar</label>
                         </div>
-                        @error('gender')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <div class="inputbox">
                         <input name="nickname"type="text" required value="{{old('nickname')}}"> <br>
                         <label for=""> Apodo </label>
-                        @error('nickname')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <div class="inputbox">
                         <input name="email" type="email" required value="{{old('email')}}"> <br>
                         <label for=""> Correo </label>
-                        @error('email')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <div class="inputbox">
                         <input name="password" type="password" required>
                         <label for=""> Contraseña </label>
-                        @error('password')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <div class="inputbox">
                         <input name="confirmpassword" type="password" required>
                         <label for=""> Confirmar Contraseña </label>
-                        @error('confirmpassword')  {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{$message}}</span> {{--print a message if there is an error--}}
-                        <br>
-                        @enderror
                     </div>
                     <button class="login-button" type="submit"> Registrarse </button>
                     <a class="back-form" href="/login">volver</a>
