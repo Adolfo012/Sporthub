33d63c2f94c16574cdd6df5e5277cd645c49dd03
Juan-Amezcua <angel.amezcua.05@gmail.com>
2023-11-23 04:05:44 -0600

    estilo a formularios de create torneos y equipos

--- resources/views/Dashboard/dashboard.blade.php
+++ resources/views/Dashboard/dashboard.blade.php
@@ -1,33 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
- {{--- Auth User ---}}
- <head>
+{{-- - Auth User - --}}
+
+<head>
     <meta charset="UTF-8">
-    <title>@yield('title')</title> {{---Add the title of the page that inherits dashboard---}}
+    <title>@yield('title')</title> {{-- -Add the title of the page that inherits dashboard- --}}
+    <link rel="stylesheet" href="{{ asset('css/torneos-create.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/equipos-create.css') }}">
     <style>
-        .active{
+        .active {
             color: red;
             font-weight: bold;
         }
     </style>
- </head>
- <body>
+</head>
 
- <p>Bienvenido {{auth()->user()->name}} {{auth()->user()->fsurname}}</p>     {{--- Show the username ---}}
- <header>
-   <h1>Dashboard</h1>
-   <nav>
-      <ul>
-         <li><a href="{{route('dashboard.index')}}" class="{{request()->routeIs('dashboard.index') ? 'active' : ''}}">Home</a></li> {{---Check if it is on the mentioned route style = active ---}}
-         <li><a href="{{route('us')}}" class="{{request()->routeIs('us') ? 'active' : ''}}">Nosotros</a> </li>
-         <li><a href="{{route('equipos.index')}}" class="{{request()->routeIs('equipos.*') ? 'active' : ''}}">Equipos</a></li> {{---Asterisk "*" indicates that the style is applied to all routes that belong to that group ---}}
-         <li><a href="{{route('torneos.index')}}" class="{{request()->routeIs('torneos.*') ? 'active' : ''}}">Torneos</a></li> {{---Asterisk "*" indicates that the style is applied to all routes that belong to that group ---}}
-      </ul>
-   </nav>
- </header>
-    <form action"logout" method="POST"> 
-    @csrf 
-                                         {{--- Alter logout GET <a href="logout">Cerrar sesion</a> ---}}
-    <a href="#" onclick="this.closest('form').submit()">Cerrar sesión</a>
-    @yield('content') {{---Add the content of the page that inherits dashboard---}}
- </body>
+<body>
+
+    <p>Bienvenido {{ auth()->user()->name }} {{ auth()->user()->fsurname }}</p> {{-- - Show the username - --}}
+    <header>
+        <h1>Dashboard</h1>
+        <nav>
+            <ul>
+                <li><a href="{{ route('dashboard.index') }}"
+                        class="{{ request()->routeIs('dashboard.index') ? 'active' : '' }}">Home</a></li>
+                {{-- -Check if it is on the mentioned route style = active - --}}
+                <li><a href="{{ route('us') }}" class="{{ request()->routeIs('us') ? 'active' : '' }}">Nosotros</a>
+                </li>
+                <li><a href="{{ route('equipos.index') }}"
+                        class="{{ request()->routeIs('equipos.*') ? 'active' : '' }}">Equipos</a></li>
+                {{-- -Asterisk "*" indicates that the style is applied to all routes that belong to that group - --}}
+                <li><a href="{{ route('torneos.index') }}"
+                        class="{{ request()->routeIs('torneos.*') ? 'active' : '' }}">Torneos</a></li>
+                {{-- -Asterisk "*" indicates that the style is applied to all routes that belong to that group - --}}
+            </ul>
+        </nav>
+    </header>
+    <form action="logout" method="POST">
+        @csrf
+        {{-- - Alter logout GET <a href="logout">Cerrar sesion</a> - --}}
+        <a href="#" onclick="this.closest('form').submit()">Cerrar sesión</a>
+        @yield('content') {{-- -Add the content of the page that inherits dashboard- --}}
+</body>
--- resources/views/Equipos/create.blade.php
+++ resources/views/Equipos/create.blade.php
@@ -1,27 +1,30 @@
-@extends('Dashboard.dashboard') {{---Inherits dashboard---}}
-@section('title','Crear equipo')
+@extends('Dashboard.dashboard') {{-- -Inherits dashboard- --}}
+@section('title', 'Crear equipo')
 
 @section('content')
-<h1>Crear Equipo</h1>
+<div class="equipos-create">
+    <h1>Crear Equipo</h1>
+        <form action="#" method="POST">
+            @csrf
+            <label>Nombre de equipo: <br>
+                <input type = "text" name="name" value="{{ old('name') }}"> <br> {{-- old() recovers what was in the field before the error --}}
+            </label>
+            @error('name')
+                {{-- Checks if there has been an error in the "name" field --}}
+                <span>*{{ $message }}</span> {{-- print a message if there is an error --}}
+                <br>
+            @enderror
+            <label>Tipo de juego del equipo: <br>
+                <input type = "text" name="tipoJuego" value="{{ old('tipoJuego') }}"> <br>
+            </label>
+            @error('tipoJuego')
+                {{-- Checks if there has been an error in the "name" field --}}
+                <span>*{{ $message }}</span> {{-- print a message if there is an error --}}
+                <br>
+            @enderror
 
-<form action="#" method="POST">
-    @csrf
-    <label>Nombre de equipo: <br>
-        <input type = "text" name="name" value="{{ old('name') }}"> <br>  {{-- old() recovers what was in the field before the error --}}
-    </label>
-    @error('name')  {{-- Checks if there has been an error in the "name" field --}}
-    <span>*{{$message}}</span> {{--print a message if there is an error--}}
-    <br>
-    @enderror
-    <label>Tipo de juego del equipo: <br>
-        <input type = "text" name="tipoJuego" value="{{ old('tipoJuego') }}"> <br>
-    </label>
-    @error('tipoJuego')  {{-- Checks if there has been an error in the "name" field --}}
-    <span>*{{$message}}</span> {{--print a message if there is an error--}}
-    <br>
-    @enderror
-    
-    <button type="submit">Crear</button>
-    
-    <a href="/equipos">Volver</a>
+            <button type="submit">Crear</button>
+
+            <a href="/equipos">Volver</a>
+    </div>
 @endsection
