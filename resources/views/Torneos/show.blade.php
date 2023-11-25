f5e51b3a09e36f72d8d59debff16d191ebd27b38
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2023-11-24 23:00:25 -0600

    Error finish

--- resources/views/Torneos/show.blade.php
+++ resources/views/Torneos/show.blade.php
@@ -15,32 +15,7 @@
         Fecha de finalizacion: {{$torneo->fechaFin}}<br> 
         Tipo de torneo: {{$torneo->tipoTorneo}}<br> 
         Cantidad de miembros admitida: {{$torneo->cantEquipo}}<br> <br> 
-        
-        Equipos registrados:
-        @php
-        $teams = 0; 
-        @endphp
-        @foreach($torneo->estadistica as $registro){{-- For each equipo in equipos--}}   
-                @if (auth()->user()->id == $torneo->user_id)
-                    <li>
-                    <a href="{{route('equipos.show',$registro)}}">  
-                    {{$registro->name}}</a>
-                     PT: {{$registro->pivot->PT}}
-                     </li> 
-                     @php
-                     $teams++;
-                     @endphp
-                @endif
-                @if($teams < 1)
-                        <p>No hay equipos aun.</p>
-                @endif
-        @endforeach
 
-        </p>
-
-        <a href="{{route('torneos.edit',$torneo)}}">Editar torneo</a>
-        <a href="{{route('partidos.index',$torneo->id)}}">Ver todos los partidos</a>
-        <a href="{{route('torneos.index')}}">Volver a torneos</a>
         @if ($torneo->tipoTorneo == "Equipos")
         @php
         $equiposTorneo = App\Models\EquipoTorneo::all(); 
