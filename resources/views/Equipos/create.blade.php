bf5420ddb80c11108389a15b69ce9f1ff0ef409b
Juan-Amezcua <angel.amezcua.05@gmail.com>
2023-11-25 13:13:05 -0600

    Mensaje errores

--- resources/views/Equipos/create.blade.php
+++ resources/views/Equipos/create.blade.php
@@ -8,26 +8,33 @@
                 <h1>Crear Equipo</h1>
                 <form action="#" method="POST">
                     @csrf
-                    <label>Nombre de equipo: <br>
-                        <input type = "text" name="name" value="{{ old('name') }}"> <br> {{-- old() recovers what was in the field before the error --}}
+                    <label>Nombre de equipo: <br><br>
+                        <input type = "text" name="name" value="{{ old('name') }}"> <br>
+                        {{-- old() recovers what was in the field before the error --}}
                     </label>
                     @error('name')
                         {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{ $message }}</span> {{-- print a message if there is an error --}}
+                        <span style="margin-top:-30px; margin-left:110px;">
+                            *{{ $message }}
+                        </span> {{-- print a message if there is an error --}}
                         <br>
                     @enderror
-                    <label>Tipo de juego del equipo: <br>
+
+                    <label>Tipo de juego del equipo: <br><br>
                         <input type = "text" name="tipoJuego" value="{{ old('tipoJuego') }}"> <br>
                     </label>
                     @error('tipoJuego')
                         {{-- Checks if there has been an error in the "name" field --}}
-                        <span>*{{ $message }}</span> {{-- print a message if there is an error --}}
+                        <span style="margin-top:-30px; margin-left:90px;">
+                            *{{ $message }}
+                        </span> {{-- print a message if there is an error --}}
                         <br>
                     @enderror
 
                     <button type="submit">Crear</button>
+                    <a href="/equipos" class="button">Volver</a>
 
-                    <a href="/equipos">Volver</a>
+                    <!-- <a href="/equipos">Volver</a> -->
                 </form>
             </div>
         </section>
